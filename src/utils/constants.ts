93c4dc7b0d5d6cad89c5a1366f081bc282c0ebd4
Maurizio Mambrini <mm.mauriziomambrini@gmail.com>
2026-03-09 23:08:26 +0100

    added android beta link

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const GOOGLE_GROUP_LINK = "https://groups.google.com/g/cowsinvaders-beta";
+export const APPLE_STORE = "https://apps.apple.com/app/cows-invaders/id6759287551";
+export const GOOGLE_STORE = "";
+export const GOOGLE_APK = "https://github.com/pixelcutter-io/cows_invaders_website/releases/download/1.1.0/cows-invaders_v1.1.0.apk";
